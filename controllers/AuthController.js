a2f4b61fd118fc8fbba508fc6f272c2da21338f5
David6676 <avetisyandavid251@gamil.com>
2023-01-31 22:43:40 +0400

    change

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { json } = require("express");
 
 
 class AuthController {
@@ -14,16 +15,14 @@ class AuthController {
         if (!validPassword) {
             return res.status(400).json({ message: "Password is not correct" })
         }
-        
-        const generateAccessToken = (user) => {
-            const payload = {
-                data: user
-            }
-            return jwt.sign(payload, process.env.SECRET, { expiresIn: "24h" })
+
+        const payload = {
+            data: user
         }
-        const token = generateAccessToken(user)
-            
-        return res.json({ token })
+
+        const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "24h" })
+
+        return res.send({ token })
     }
 };
 
--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -1,15 +1,18 @@
 const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
 
 module.exports = function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
+    console.log(authHeader);
     const token = authHeader && authHeader.split(' ')[1]
 
     if (token == null) {
         return res.sendStatus(401)
     }
 
-    jwt.verify(token, process.env.SECRET, () => {
-        console.log(err)
+    jwt.verify(token, process.env.SECRET, async () => {
+        const user = await Users.findByPk(token.data.id);
+        console.log(token)
 
         if (err) {
             return res.sendStatus(403)
