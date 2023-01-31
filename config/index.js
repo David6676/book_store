a1c32a07a3711b4e8ee85b345969fa56e58f6193
David6676 <avetisyandavid251@gamil.com>
2023-01-31 14:47:12 +0400

    addition of keys in the .env file

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "book_store",
-    DIALECT: "mysql"
-};
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB,
+    DIALECT: process.env.DIALECT
+}
